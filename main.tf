6b7eb0172e06321c70bfaed0aa905d58e172b580
ChengaDev <gazit.chen@gmail.com>
2022-08-04 16:24:04 +0300

    remove aws_db_instance

--- main.tf
+++ main.tf
@@ -36,21 +36,20 @@ resource "aws_api_gateway_rest_api" "spec_api_gateway" {
   }
 }
 
-resource "aws_db_instance" "spec_db" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  name                 = "mydb"
-  username             = "foo"
-  password             = "foobarbaz"
-  iam_database_authentication_enabled = true
-  storage_encrypted = true
-  ca_cert_identifier = "rds-ca-2019"
-  ### TFAWS039P ###
-  auto_minor_version_upgrade = false
-}
+# resource "aws_db_instance" "spec_db" {
+#   allocated_storage    = 20
+#   storage_type         = "gp2"
+#   engine               = "mysql"
+#   engine_version       = "5.7"
+#   instance_class       = "db.t2.micro"
+#   name                 = "mydb"
+#   username             = "foo"
+#   password             = "foobarbaz"
+#   iam_database_authentication_enabled = true
+#   storage_encrypted = true
+#   ca_cert_identifier = "rds-ca-2019"
+#   auto_minor_version_upgrade = true
+# }
 
 resource "aws_s3_bucket" "spec_bucket" {
     bucket = "my-tf-test-bucket"
