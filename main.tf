658a1e54f24f253a393b46bddd33eb2cf2b6d589
ChengaDev <gazit.chen@gmail.com>
2022-08-04 15:06:30 +0300

    make endpoint private + encrypt ebs_block_device

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_ami" "spec_ami" {
     snapshot_id = "snap-xxxxxxxx"
     volume_size = 8
     ### TFAWS007P ###
-    # encrypted = true
+    encrypted = true
   }
 }
 
@@ -31,9 +31,9 @@ resource "aws_api_gateway_rest_api" "spec_api_gateway" {
   name = "regional-example"
 
   ### TFAWS016P ###
-  # endpoint_configuration {
-  #   types = ["PRIVATE"]
-  # }
+  endpoint_configuration {
+    types = ["PRIVATE"]
+  }
 }
 
 resource "aws_db_instance" "spec_db" {
